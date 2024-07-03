d47b7d0b59b38c51f8c1d2af921cf513f2f83e97
Gabriel Borglund <gabrielb@spotify.com>
2024-07-03 15:41:18 +0200

    Add steps to reproduce the crash

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,7 +68,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.0"
+        kotlinCompilerExtensionVersion = "1.5.5"
     }
     packaging {
         resources {
